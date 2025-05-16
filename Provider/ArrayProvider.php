28adc305a4a09b9d68ef406698a36c691d3c2e7e
Jisse Reitsma <jisse@yireo.com>
2025-05-16 06:28:58 +0200

    Adding all files to git via Yireo Command

---
+++ Provider/ArrayProvider.php
@@ -0,0 +1,48 @@
+<?php
+declare(strict_types=1);
+
+namespace YireoTraining\ExampleLokiAdminArrayGrid\Provider;
+
+use GuzzleHttp\Client;
+use Yireo\LokiAdminComponents\Provider\ArrayProviderInterface;
+
+class ArrayProvider implements ArrayProviderInterface
+{
+    public function __construct(
+        private Client $client
+    ) {
+    }
+
+    public function getColumns(): array
+    {
+        return [
+            'package_name' => 'Composer package',
+            'version' => 'Version',
+            'release_date' => 'Release date'
+        ];
+    }
+
+    public function getData(): array
+    {
+        $response = $this->client->get('https://composer.yireo.com/packages.json');
+        $composerData = json_decode($response->getBody()->getContents(), true);
+
+        $rows = [];
+
+        foreach ($composerData['packages'] as $packageName => $packageVersions) {
+            foreach ($packageVersions as $packageVersion) {
+                if (str_starts_with($packageVersion['version'], 'dev-')) {
+                    continue;
+                }
+
+                $rows[] = [
+                    'package_name' => $packageName,
+                    'version' => $packageVersion['version'],
+                    'release_date' => $packageVersion['release_date'],
+                ];
+            }
+        }
+
+        return $rows;
+    }
+}
